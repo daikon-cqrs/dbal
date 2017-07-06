c10f8aba85c26555a4facfc5a0df97d8bf71e51c
Hasham Ahmad <mr.hash@gmail.com>
2017-07-06 12:59:44 +0100

    Add RepositoryInterface

---
+++ src/Repository/RepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Daikon\Dbal\Repository;
+
+use Dailex\Infrastructure\Projection\ProjectionInterface;
+use Dailex\Infrastructure\Projection\ProjectionMap;
+
+interface RepositoryInterface
+{
+    public function findById(string $identifier): ProjectionInterface;
+
+    public function findByIds(array $identifiers): ProjectionMap;
+
+    public function persist(ProjectionInterface $projection): bool;
+}
